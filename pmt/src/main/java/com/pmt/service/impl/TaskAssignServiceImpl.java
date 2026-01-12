85fa75c0b2608e162f590a553d1308b32f4d3a50
clementuu <clementcalia@gmail.com>
2026-01-12 17:52:29 +0100

    feat(pmt) envoie de mail a l'assignation à une tâche

--- pmt/src/main/java/com/pmt/service/impl/TaskAssignServiceImpl.java
+++ pmt/src/main/java/com/pmt/service/impl/TaskAssignServiceImpl.java
@@ -12,6 +12,7 @@ import com.pmt.errors.ValidationException;
 import com.pmt.model.Task;
 import com.pmt.model.TaskAssign;
 import com.pmt.model.User;
+import com.pmt.service.EmailService;
 import com.pmt.service.TaskAssignService;
 import com.pmt.store.TaskAssignStore;
 import com.pmt.store.TaskStore;
@@ -25,18 +26,36 @@ public class TaskAssignServiceImpl implements TaskAssignService {
     UserStore userStore;
     @Autowired
     TaskStore taskStore;
+    @Autowired
+    EmailService emailService;
 
     @Override
     public TaskAssign create(Long taskId, Long userId) {
-        TaskAssign taskAssign = new TaskAssign();
         Task task = taskStore.findById(taskId)
                 .orElseThrow(() -> new ValidationException("Task not found with ID: " + taskId));
         User user = userStore.findById(userId)
                 .orElseThrow(() -> new ValidationException("User not found with ID: " + userId));
         
+        TaskAssign taskAssign = new TaskAssign();
         taskAssign.setTask(task);
         taskAssign.setUser(user);
-        return taskAssignStore.save(taskAssign);
+        
+        TaskAssign savedTaskAssign = taskAssignStore.save(taskAssign);
+
+        try {
+            String to = user.getEmail();
+            String subject = "Nouvelle tâche assignée : " + task.getNom();
+            String text = "Bonjour " + user.getNom() + ",\n\n"
+                        + "Vous avez été assigné(e) à une nouvelle tâche : '" + task.getNom() + "'.\n\n"
+                        + "L'équipe PMT.";
+
+            emailService.sendSimpleMessage(to, subject, text);
+            System.out.println("Email de notification envoyé à " + to);
+        } catch (Exception e) {
+            System.err.println("Erreur lors de l'envoi de l'email de notification à " + user.getEmail() + e);
+        }
+
+        return savedTaskAssign;
     }
 
     @Override
